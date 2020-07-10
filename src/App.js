709294821238fbdb0d8952086a1a17d8519efcf0
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2020-07-10 11:54:26 -0700

    Add proof of concept

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import EmojiFader from './EmojiFader'
+import './App.css'
+
+const mages = ['🧙‍♀️', '🧙🏻‍♀️', '🧙🏼‍♀️', '🧙🏽‍♀️', '🧙🏾‍♀️', '🧙🏿‍♀️']
+const wizards = ['🧙‍♂️', '🧙🏻‍♂️', '🧙🏼‍♂️', '🧙🏽‍♂️', '🧙🏾‍♂️', '🧙🏿‍♂️']
+const apples = ['🍎️', '🍏️']
+// String.fromCharCode('200D')
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>emoji-fader</h1>
+			<div>This is not an efficient method for doing this. This was only a test.</div>
+
+			<h2>Basic Usage</h2>
+			<div className="font-size-parent">
+				<EmojiFader emojis={wizards} />
+				<EmojiFader emojis={mages} />
+				<EmojiFader emojis={apples} />
+			</div>
+
+			<h2>Interval</h2>
+			<div className="font-size-parent">
+				<EmojiFader emojis={apples} interval={500} />
+			</div>
+
+			<h2>Alignment Test</h2>
+			<h3>Flex Row</h3>
+			<div className="flex-row font-size-parent">
+				<EmojiFader emojis={wizards} />
+				<EmojiFader emojis={mages} />
+			</div>
+
+			<h3>Centered Over Text</h3>
+			<div className="flex-column font-size-parent">
+				<EmojiFader emojis={wizards} />
+				<div className="dummy-text">Congratulations!</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/EmojiFader.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './EmojiFader.css'
+
+const nextIndex = (i, listLength) => (i + 1 === listLength ? 0 : i + 1)
+
+const EmojiFader = ({ emojis, interval = 1000 }) => {
+	// Tracks the visible index
+	const [index, setIndex] = React.useState(0)
+
+	// Timer
+	React.useEffect(() => {
+		const timer = setTimeout(() => {
+			setIndex(nextIndex(index, emojis.length))
+		}, interval)
+		return () => {
+			clearTimeout(timer)
+		}
+	}, [emojis.length, index, interval])
+
+	return (
+		<span className="icon-wrapper">
+			<span className="icon">{emojis[nextIndex(index, emojis.length)]}</span>
+			{/* Current index below next so that it fades into it */}
+			<span className="icon fade-out" style={{ animationDuration: `${interval}ms` }} key={index}>
+				{emojis[index]}
+			</span>
+		</span>
+	)
+}
+
+EmojiFader.propTypes = {
+	wizard: PropTypes.bool,
+}
+
+export default EmojiFader
